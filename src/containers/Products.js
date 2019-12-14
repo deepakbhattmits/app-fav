bea7b8393a4c1cb1e1c9c97e7db357deefb385ae
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-14 22:21:53 +0530

    used context

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-
+import React, { useContext } from 'react';
+// import { useSelector } from 'react-redux';
+import { ProductsContext } from '../context/products-context';
 import ProductItem from '../components/Products/ProductItem';
 import './Products.css';
 
 const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+  const productsList = useContext(ProductsContext).products;
+  // const productList = useSelector(state => state.shop.products);
   return (
     <ul className='products-list'>
-      {productList.map(({ id, title, description, isFavorite }) => (
+      {productsList.map(({ id, title, description, isFavorite }) => (
         <ProductItem
           key={id}
           id={id}
