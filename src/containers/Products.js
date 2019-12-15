ae2deae71bada91089798cf06965cc640b94960b
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-15 12:36:24 +0530

    custom react state management

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,15 +1,19 @@
-import React, { useContext } from 'react';
+import React from 'react';
+// import React, { useContext } from 'react';
 // import { useSelector } from 'react-redux';
-import { ProductsContext } from '../context/products-context';
+// import { ProductsContext } from '../context/products-context';
+import { useStore } from '../hooks-store/store';
 import ProductItem from '../components/Products/ProductItem';
 import './Products.css';
 
 const Products = props => {
-  const productsList = useContext(ProductsContext).products;
+  // const productsList = useContext(ProductsContext).products;
+
   // const productList = useSelector(state => state.shop.products);
+  const state = useStore()[0];
   return (
     <ul className='products-list'>
-      {productsList.map(({ id, title, description, isFavorite }) => (
+      {state.products.map(({ id, title, description, isFavorite }) => (
         <ProductItem
           key={id}
           id={id}
