1402905a385433dbafc78bb0bc1068a4ca61eb7e
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-15 12:36:10 +0530

    custom react state management

--- src/containers/Favorites.js
+++ src/containers/Favorites.js
@@ -1,6 +1,8 @@
-import React, { useContext } from 'react';
+import React from 'react';
+// import React, { useContext } from 'react';
 // import { useSelector } from 'react-redux';
-import { ProductsContext } from '../context/products-context';
+// import { ProductsContext } from '../context/products-context';
+import { useStore } from '../hooks-store/store';
 import FavoriteItem from '../components/Favorites/FavoriteItem';
 import './Products.css';
 
@@ -8,19 +10,21 @@ const Favorites = props => {
   // const favoriteProducts = useSelector(state =>
   //   state.shop.products.filter(p => p.isFavorite)
   // );
-  const favoriteProducts = useContext(ProductsContext).products.filter(
-    p => p.isFavorite
-  );
+  // const favoriteProducts = useContext(ProductsContext).products.filter(
+  //   p => p.isFavorite
+  // );
+  const state = useStore()[0];
+  const favoriteProducts = state.products.filter(p => p.isFavorite);
   let content = <p className='placeholder'>Got no favorites yet!</p>;
   if (favoriteProducts.length > 0) {
     content = (
       <ul className='products-list'>
-        {favoriteProducts.map(prod => (
+        {favoriteProducts.map(({ id, title, description }) => (
           <FavoriteItem
-            key={prod.id}
-            id={prod.id}
-            title={prod.title}
-            description={prod.description}
+            key={id}
+            id={id}
+            title={title}
+            description={description}
           />
         ))}
       </ul>
